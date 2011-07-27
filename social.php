0874c5d1e54a7aa4f50b146f53b49f056a460ff7
Jared Cornwall <jared@crowdfavorite.com>
2011-07-27 21:58:31 +0000

    Copy adjustments

--- social.php
+++ social.php
@@ -812,7 +812,7 @@ final class Social {
         global $post;
 ?>
 <div class="social-meta-box-block">
-	<h4><?php _e('Add by URL', Social::$i18n); ?></h4>
+	<h4><?php _e('Add Tweet by URL', Social::$i18n); ?></h4>
 	<p><?php _e('Want to add a tweet? Enter the URL of the tweet here and Social will add it as a comment.', Social::$i18n); ?></p>
 	<p>
 	    <input type="text" name="source_url" style="width:350px" />
@@ -825,7 +825,7 @@ final class Social {
 
 <div class="social-meta-box-block cf-clearfix">
 	<h4><?php _e('Manual Refresh', Social::$i18n); ?></h4>
-	<p><?php _e('You can manually run the comment aggregation by clicking the button below.', Social::$i18n); ?></p>
+	<p><?php _e('Manually run the comment aggregation and Social will look for mentions of this post on Facebook and Twitter.', Social::$i18n); ?></p>
 	<p class="submit" style="clear:both;float:none;padding:0;">
 	    <a href="<?php echo wp_nonce_url(admin_url('?social_action=run_aggregation&post_id='.$post->ID)); ?>" id="run_aggregation" class="button" style="float:left;margin-bottom:10px;"><?php _e('Find Social Comments', Social::$i18n); ?></a>
 	    <img src="<?php echo admin_url('images/loading.gif'); ?>" style="float:left;position:relative;top:4px;left:5px;display:none;" id="run_aggregation_loader" />
