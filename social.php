8d31ad595bc32363406d3fe454602afa9042f6e5
Jared Cornwall <jared@crowdfavorite.com>
2011-07-27 21:45:30 +0000

    more styling

--- social.php
+++ social.php
@@ -811,27 +811,33 @@ final class Social {
     public function add_meta_log_box() {
         global $post;
 ?>
-<h4 style="margin:20px 0 7px 0;"><?php _e('Add by URL', Social::$i18n); ?></h4>
-<p><?php _e('Want to add a tweet? Enter the URL of the tweet here and Social will add the tweet as a comment.', Social::$i18n); ?></p>
-<p>
-    <input type="text" name="source_url" style="width:350px" />
-    <span class="submit" style="float:none">
-        <a href="<?php echo wp_nonce_url(admin_url('?social_action=import_from_url&post_id='.$post->ID)); ?>" id="import_from_url" class="button"><?php _e('Import Tweet', Social::$i18n); ?></a>
-    </span>
-    <img src="<?php echo admin_url('images/loading.gif'); ?>" style="position:relative;top:4px;left:0;display:none" id="import_from_url_loader" />
-</p>
-<h4 style="margin:20px 0 7px 0;"><?php _e('Manual Refresh', Social::$i18n); ?></h4>
-<p><?php _e('You can manually run the comment aggregation by clicking the button below.', Social::$i18n); ?></p>
-<p class="submit" style="clear:both;float:none;padding:0;">
-    <a href="<?php echo wp_nonce_url(admin_url('?social_action=run_aggregation&post_id='.$post->ID)); ?>" id="run_aggregation" class="button" style="float:left;margin-bottom:15px;"><?php _e('Find Social Comments', Social::$i18n); ?></a>
-    <img src="<?php echo admin_url('images/loading.gif'); ?>" style="float:left;position:relative;top:4px;left:5px;display:none;" id="run_aggregation_loader" />
-    <div style="clear:both"></div>
-</p>
+<div class="social-meta-box-block">
+	<h4><?php _e('Add by URL', Social::$i18n); ?></h4>
+	<p><?php _e('Want to add a tweet? Enter the URL of the tweet here and Social will add it as a comment.', Social::$i18n); ?></p>
+	<p>
+	    <input type="text" name="source_url" style="width:350px" />
+	    <span class="submit" style="float:none">
+	        <a href="<?php echo wp_nonce_url(admin_url('?social_action=import_from_url&post_id='.$post->ID)); ?>" id="import_from_url" class="button"><?php _e('Import Tweet', Social::$i18n); ?></a>
+	    </span>
+	    <img src="<?php echo admin_url('images/loading.gif'); ?>" style="position:relative;top:4px;left:0;display:none" id="import_from_url_loader" />
+	</p>
+</div><!-- .social-meta-box-block -->
 
-<h4 style="margin:0 0 7px 0;"><?php _e('Log', Social::$i18n); ?></h4>
-<div id="aggregation_log">
-    <?php echo Social_Aggregate_Log::logs($post->ID); ?>
-</div>
+<div class="social-meta-box-block cf-clearfix">
+	<h4><?php _e('Manual Refresh', Social::$i18n); ?></h4>
+	<p><?php _e('You can manually run the comment aggregation by clicking the button below.', Social::$i18n); ?></p>
+	<p class="submit" style="clear:both;float:none;padding:0;">
+	    <a href="<?php echo wp_nonce_url(admin_url('?social_action=run_aggregation&post_id='.$post->ID)); ?>" id="run_aggregation" class="button" style="float:left;margin-bottom:10px;"><?php _e('Find Social Comments', Social::$i18n); ?></a>
+	    <img src="<?php echo admin_url('images/loading.gif'); ?>" style="float:left;position:relative;top:4px;left:5px;display:none;" id="run_aggregation_loader" />
+	</p>
+</div><!-- .social-meta-box-block -->
+
+<div class="social-meta-box-block">
+	<h4><?php _e('Log', Social::$i18n); ?></h4>
+	<div id="aggregation_log">
+	    <?php echo Social_Aggregate_Log::logs($post->ID); ?>
+	</div>
+</div><!-- .social-meta-box-block -->
 <?php
     }
 
