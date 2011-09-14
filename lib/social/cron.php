acd7c9a87ab20b124b93c990589a772e9f83900e
Andrew Ellis <awellis@me.com>
2011-09-14 16:04:46 -0600

    Fixed a bug that caused the wrong cron lock directory to be used.

--- lib/social/cron.php
+++ lib/social/cron.php
@@ -135,7 +135,7 @@ final class Social_CRON {
 	 */
 	private function lock() {
 		$locked = false;
-		$file = trailingslashit(Social::$cron_lock_dir).$this->_key.'.txt';
+		$file = trailingslashit($this->_cron_lock_dir).$this->_key.'.txt';
 
 		$timestamp = 0;
 		if (is_file($file)) {
@@ -168,7 +168,7 @@ final class Social_CRON {
 	 */
 	private function unlock() {
 		$unlocked = false;
-		$file = trailingslashit(Social::$cron_lock_dir).$this->_key.'.txt';
+		$file = trailingslashit($this->_cron_lock_dir).$this->_key.'.txt';
 
 		try {
 			$fp = fopen($file, 'r+');
