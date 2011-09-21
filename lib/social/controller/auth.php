020a74bded1ffe4d5e0e2edc0f4dc9e10fb97678
Andrew Ellis <awellis@me.com>
2011-09-21 12:44:03 -0600

    Logging for auth

--- lib/social/controller/auth.php
+++ lib/social/controller/auth.php
@@ -22,6 +22,7 @@ final class Social_Controller_Auth extends Social_Controller {
 			'keys' => $data->keys,
 			'user' => $data->user
 		);
+		Social::log('Account Pre KSES: '.print_r($account->user, true));
 		$account->user = $this->social->kses($account->user);
 
 		$class = 'Social_Service_'.$data->service.'_Account';
