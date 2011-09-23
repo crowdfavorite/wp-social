25bee6a1f0b4e132570697ae184779adec3e04a5
Andrew Ellis <awellis@me.com>
2011-09-23 11:01:50 -0600

    Fixed typo

--- views/wp-admin/post/broadcast/error/notice.php
+++ views/wp-admin/post/broadcast/error/notice.php
@@ -1,6 +1,6 @@
 <?php
 
-echo '<p>'.sprintf(__('Social failed to broadcast the blog post "%s" to one ore more of your Social accounts.', Social::$i18n), esc_html($post->post_title)).'</p>';
+echo '<p>'.sprintf(__('Social failed to broadcast the blog post "%s" to one or more of your Social accounts.', Social::$i18n), esc_html($post->post_title)).'</p>';
 
 foreach ($accounts as $key => $items) {
 	echo '<h4>'.$social->service($key)->title().':</h4><ul>';
