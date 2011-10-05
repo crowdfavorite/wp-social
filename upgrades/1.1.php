52057d2cc48182ed99e5ea9124b2c6b45c589454
Andrew Ellis <awellis@me.com>
2011-10-05 13:36:53 -0600

    Formatting changes

--- upgrades/1.1.php
+++ upgrades/1.1.php
@@ -1,4 +1,4 @@
-<?php if (!defined('SOCIAL_UPGRADE')) die('Direct script access not allowed.');
+<?php if (!defined('SOCIAL_UPGRADE')) { die('Direct script access not allowed.'); }
 /**
  * Upgrades Social to 1.1.
  */
@@ -88,15 +88,15 @@ if (version_compare($installed_version, '1.1', '<')) {
 		 WHERE option_name = 'social_system_crons'
 	");
 
-    if (empty($fetch)) {
-        $fetch = '1';
-    }
-    
-    $wpdb->query("
-        INSERT
-          INTO $wpdb->options (option_name, option_value)
-        VALUES('social_fetch_comments', '$fetch')
-            ON DUPLICATE KEY UPDATE option_id = option_id
+	if (empty($fetch)) {
+		$fetch = '1';
+	}
+
+	$wpdb->query("
+		INSERT
+		  INTO $wpdb->options (option_name, option_value)
+		VALUES('social_fetch_comments', '$fetch')
+		    ON DUPLICATE KEY UPDATE option_id = option_id
     ");
 
 	// Update all comment types
@@ -115,70 +115,70 @@ if (version_compare($installed_version, '1.1', '<')) {
 			 WHERE comment_type = '$key'
 		");
 	}
-}
 
-// Make sure all commenter accounts have the commenter flag
-$results = $wpdb->get_results("
-SELECT m.user_id
-  FROM $wpdb->users AS u
-  JOIN $wpdb->usermeta AS m
-    ON m.user_id = u.ID
- WHERE m.meta_key = 'social_accounts'
-   AND u.user_email LIKE '%@example.com'
-");
-if (is_array($results)) {
-	foreach ($results as $result) {
-		update_user_meta($result->user_id, 'social_commenter', 'true');
+	// Make sure all commenter accounts have the commenter flag
+	$results = $wpdb->get_results("
+		SELECT m.user_id
+		  FROM $wpdb->users AS u
+		  JOIN $wpdb->usermeta AS m
+		    ON m.user_id = u.ID
+		 WHERE m.meta_key = 'social_accounts'
+		   AND u.user_email LIKE '%@example.com'
+    ");
+	if (is_array($results)) {
+		foreach ($results as $result) {
+			update_user_meta($result->user_id, 'social_commenter', 'true');
+		}
 	}
-}
 
-// Rename the XMLRPC option
-$wpdb->query("
-UPDATE $wpdb->options
-   SET option_name = 'social_default_accounts'
- WHERE option_name = 'social_xmlrpc_accounts'
-");
+	// Rename the XMLRPC option
+	$wpdb->query("
+		UPDATE $wpdb->options
+		   SET option_name = 'social_default_accounts'
+		 WHERE option_name = 'social_xmlrpc_accounts'
+    ");
 
-// Fix the broadcasted IDs format
-$results = $wpdb->get_results("
-SELECT meta_value, post_id
-  FROM $wpdb->postmeta
- WHERE meta_key = '_social_broadcasted_ids'
-");
-if (is_array($results)) {
-	foreach ($results as $result) {
-	    $meta_value = maybe_unserialize($result->meta_value);
-	    if (is_array($meta_value)) {
-	        $_meta_value = array();
-	        foreach ($meta_value as $service_key => $accounts) {
-	            if (!isset($_meta_value[$service_key])) {
-	                $_meta_value[$service_key] = array();
-	            }
+	// Fix the broadcasted IDs format
+	$results = $wpdb->get_results("
+		SELECT meta_value, post_id
+		  FROM $wpdb->postmeta
+		 WHERE meta_key = '_social_broadcasted_ids'
+    ");
+	if (is_array($results)) {
+		foreach ($results as $result) {
+			$meta_value = maybe_unserialize($result->meta_value);
+			if (is_array($meta_value)) {
+				$_meta_value = array();
+				foreach ($meta_value as $service_key => $accounts) {
+					if (!isset($_meta_value[$service_key])) {
+						$_meta_value[$service_key] = array();
+					}
 
-	            foreach ($accounts as $account_id => $broadcasted) {
-	                if (!isset($_meta_value[$service_key][$account_id])) {
-	                    $_meta_value[$service_key][$account_id] = array();
-	                }
+					foreach ($accounts as $account_id => $broadcasted) {
+						if (!isset($_meta_value[$service_key][$account_id])) {
+							$_meta_value[$service_key][$account_id] = array();
+						}
 
-	                if (is_array($broadcasted)) {
-		                foreach ($broadcasted as $id => $data) {
-		                    if ((int) $data) {
-		                        $_meta_value[$service_key][$account_id][$data] = '';
-		                    }
-		                    else {
-		                        $_meta_value[$service_key][$account_id][$id] = $data;
-		                    }
-		                }
-	                }
-	                else {
-		                $_meta_value[$service_key][$account_id][$broadcasted] = '';
-	                }
-	            }
-	        }
+						if (is_array($broadcasted)) {
+							foreach ($broadcasted as $id => $data) {
+								if ((int) $data) {
+									$_meta_value[$service_key][$account_id][$data] = '';
+								}
+								else {
+									$_meta_value[$service_key][$account_id][$id] = $data;
+								}
+							}
+						}
+						else {
+							$_meta_value[$service_key][$account_id][$broadcasted] = '';
+						}
+					}
+				}
 
-	        if (!empty($_meta_value)) {
-	            update_post_meta($result->post_id, '_social_broadcasted_ids', $_meta_value);
-	        }
-	    }
+				if (!empty($_meta_value)) {
+					update_post_meta($result->post_id, '_social_broadcasted_ids', $_meta_value);
+				}
+			}
+		}
 	}
 }
