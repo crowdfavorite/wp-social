7cd6ba902c736a179f1e73547e164b8f66de9131
Andrew Ellis <awellis@me.com>
2011-11-01 14:40:50 -0600

    Shortened non-broadcasted copy

--- views/wp-admin/post/meta/broadcast/published.php
+++ views/wp-admin/post/meta/broadcast/published.php
@@ -1,10 +1,6 @@
 <?php
 if (!$broadcasted) {
 	echo '<p class="mar-top-none">'
-	   . __('This post has not been broadcasted to any accounts yet. You may do so by clicking the "Broadcast" button below.', 'social')
+	   . __('This post has not been broadcasted, click "Broadcast" to select accounts and broadcast this post.', 'social')
 	  . '</p>';
-
-	if (!is_array($ids) or !count($ids)) {
-		echo '<p>'.__('Would you like to broadcast this post?', 'social').'</p>';
-	}
 }
