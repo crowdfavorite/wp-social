1c95fcab125c7dc922f7bfb2f19dda76931b7fdf
Andrew Ellis <awellis@me.com>
2011-11-09 15:34:47 -0700

    Fixed the email format

--- views/wp-admin/post/broadcast/error/email.php
+++ views/wp-admin/post/broadcast/error/email.php
@@ -1,15 +1,21 @@
 <?php
-_e('Hello', 'social').','."\n\n";
+$deauthed = false;
+echo __('Hello', 'social').','."\n\n";
 echo wordwrap(sprintf(__('Social failed to broadcast the blog post "%s" to one or more of your Social accounts.', 'social'), $post->post_title), 60)."\n\n";
 foreach ($accounts as $key => $items) {
 	foreach ($items as $item) {
+		if (isset($item->deauthed)) {
+			$deauthed = true;
+		}
 		echo $social->service($key)->title().': '.$item->account->name().' ('.$item->reason.')'."\n";
 	}
 	echo "\n";
 }
 
-echo wordwrap(__('Please login and reauthenticate the above accounts if you wish to continue using them.', 'social'), 60)."\n\n";
-_e('Global accounts:', 'social')."\n";
-echo esc_url(Social::settings_url())."\n";
-_e('Personal accounts:', 'social')."\n";
-echo esc_url(admin_url('profile.php#social-accounts'))."\n\n";
+if ($deauthed) {
+	echo wordwrap(__('Please login and reauthenticate the above accounts if you wish to continue using them.', 'social'), 60)."\n\n";
+	_e('Global accounts: ', 'social')."\n";
+	echo esc_url(Social::settings_url())."\n";
+	_e('Personal accounts: ', 'social')."\n";
+	echo esc_url(admin_url('profile.php#social-accounts'))."\n\n";
+}
