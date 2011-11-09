5cfc99d868c1366f3a80ad41d62ef22b3057a53e
Andrew Ellis <awellis@me.com>
2011-11-09 14:10:52 -0700

    Formatting of the error email updated

--- views/wp-admin/post/broadcast/error/email.php
+++ views/wp-admin/post/broadcast/error/email.php
@@ -1,16 +1,15 @@
 <?php
 _e('Hello', 'social').','."\n\n";
-echo wordwrap(sprintf(__('Social failed to broadcast the blog post "%s" to one or more of your Social accounts.', 'social'), esc_html($post->post_title)), 60)."\n\n";
+echo wordwrap(sprintf(__('Social failed to broadcast the blog post "%s" to one or more of your Social accounts.', 'social'), $post->post_title), 60)."\n\n";
 foreach ($accounts as $key => $items) {
-	echo esc_html($social->service($key)->title()).':'."\n";
 	foreach ($items as $item) {
-		echo '- '.esc_html($item->account->name())."\n";
+		echo $social->service($key)->title().': '.$item->account->name().' ('.$item->reason.')'."\n";
 	}
 	echo "\n";
 }
 
 echo wordwrap(__('Please login and reauthenticate the above accounts if you wish to continue using them.', 'social'), 60)."\n\n";
 _e('Global accounts:', 'social')."\n";
-echo esc_url(Social::settings_url())."\n\n";
+echo esc_url(Social::settings_url())."\n";
 _e('Personal accounts:', 'social')."\n";
 echo esc_url(admin_url('profile.php#social-accounts'))."\n\n";
