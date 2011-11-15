7f7d851a0076f98643920ef031157b678f33e0c9
Andrew Ellis <awellis@me.com>
2011-11-15 11:43:15 -0700

    Stupid mistake...

--- lib/social/service.php
+++ lib/social/service.php
@@ -628,7 +628,7 @@ abstract class Social_Service {
 		}
 
 		$status_url = $this->status_url($item->comment_author, $item->social_status_id);
-		$title = apply_filters('social_item_output_title', $item->comment_author, $this->key);
+		$title = apply_filters('social_item_output_title', $item->comment_author, $this->key());
 		$image = sprintf('<img src="%s" width="%s" height="%s" alt="%s" />', esc_url($item->social_profile_image_url), esc_attr($width), esc_attr($height), esc_attr($title));
 		return sprintf('<a href="%s" title="%s"%s>%s</a>', esc_url($status_url), esc_attr($title), $style, $image);
 	}
