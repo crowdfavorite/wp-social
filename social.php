530228439efa85fb68f2d99ab10a7489fb7ec2be
Alex King <alex@alexking.org>
2011-11-16 15:24:59 -0700

    bumping version number

--- social.php
+++ social.php
@@ -3,7 +3,7 @@
 Plugin Name: Social
 Plugin URI: http://mailchimp.com/social-plugin-for-wordpress/
 Description: Broadcast newly published posts and pull in discussions using integrations with Twitter and Facebook. Brought to you by <a href="http://mailchimp.com">MailChimp</a>.
-Version: 2.0b1
+Version: 2.0b2
 Author: Crowd Favorite
 Author URI: http://crowdfavorite.com/
 */
