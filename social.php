9025bb697b3db6cd15f3e86dc7d56ba7ad024b71
Jonathan Johnson <jon@crowdfavorite.com>
2012-10-12 16:04:13 -0600

    bump version in plugin file

--- social.php
+++ social.php
@@ -3,7 +3,7 @@
 Plugin Name: Social
 Plugin URI: http://mailchimp.com/social-plugin-for-wordpress/
 Description: Broadcast newly published posts and pull in discussions using integrations with Twitter and Facebook. Brought to you by <a href="http://mailchimp.com">MailChimp</a>.
-Version: 2.5
+Version: 2.6rc2
 Author: Crowd Favorite
 Author URI: http://crowdfavorite.com/
 */
