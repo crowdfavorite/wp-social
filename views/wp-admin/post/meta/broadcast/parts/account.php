e120455f51e758f762b350d5c8fe93993d8ff346
Alex King <alex@alexking.org>
2013-07-03 11:27:47 -0600

    proper fix

--- views/wp-admin/post/meta/broadcast/parts/account.php
+++ views/wp-admin/post/meta/broadcast/parts/account.php
@@ -2,11 +2,13 @@
 	<img src="<?php echo esc_url($account->avatar()); ?>" width="24" height="24" />
 	<span>
 <?php
-$broadcasted = $service->title();
+$broadcasted = esc_html($service->title());
 if (isset($broadcasted_id)) {
 	if ($account->has_user() or $service->key() != 'twitter') {
 		$url = $service->status_url($account->username(), $broadcasted_id);
-		$broadcasted = (empty($url) ? esc_html($service->title()) : '<a href="'.esc_url($url).'" target="_blank">'.esc_html($service->title()).'</a>');
+		if (!empty($url)) {
+			$broadcasted = '<a href="'.esc_url($url).'" target="_blank">'.esc_html($service->title()).'</a>';
+		}
 	}
 }
 echo esc_html($account->name()).' &middot; '.$broadcasted;
