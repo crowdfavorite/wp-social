f23e40fc91da4397b73d965cbac7812f98fd5732
Chris Mospaw <mospaw@crowdfavorite.com>
2014-10-10 10:47:19 -0600

    change Facebook max_broacast_length to 25000 (from 420)

--- lib/social/service/facebook.php
+++ lib/social/service/facebook.php
@@ -20,7 +20,7 @@ final class Social_Service_Facebook extends Social_Service implements Social_Int
 	 * @return int
 	 */
 	public function max_broadcast_length() {
-		return 420;
+		return 25000;
 	}
 
 	/**
