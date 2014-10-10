c792604b9fbb16d12f2e3d9e0ff3a9506141362f
Chris Mospaw <mospaw@crowdfavorite.com>
2014-10-10 14:20:44 -0600

    change replacement character

--- assets/admin.js
+++ assets/admin.js
@@ -179,7 +179,7 @@
 
 		$('.broadcast-interstitial .broadcast-edit textarea').on('keyup change click focus', function() {
 			$counter = $(this).closest('.broadcast-edit').find('.counter');
-			$(this).val($(this).val().replace(/\n|\r/ig,' '));
+			$(this).val($(this).val().replace(/\n|\r/ig,"\n"));
 			var diff = parseInt($(this).attr('maxlength')) - parseInt($(this).val().replace(/\n|\r/ig,' ').length),
 				diffClass = '';
 			if (diff < 10) {
