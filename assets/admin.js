245f7331ddc27ca0000dfeb46f829e5b4bc45f1e
Chris Mospaw <mospaw@crowdfavorite.com>
2014-10-10 14:19:27 -0600

    Further modification of counter limitation.

--- assets/admin.js
+++ assets/admin.js
@@ -179,6 +179,7 @@
 
 		$('.broadcast-interstitial .broadcast-edit textarea').on('keyup change click focus', function() {
 			$counter = $(this).closest('.broadcast-edit').find('.counter');
+			$(this).val($(this).val().replace(/\n|\r/ig,' '));
 			var diff = parseInt($(this).attr('maxlength')) - parseInt($(this).val().replace(/\n|\r/ig,' ').length),
 				diffClass = '';
 			if (diff < 10) {
