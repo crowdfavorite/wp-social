0b2619fa91f7e159dd8d4cb6c57eb4953c5e91ef
Chris Mospaw <mospaw@crowdfavorite.com>
2014-10-10 11:50:32 -0600

    Remove debug code. Increase Facebook max_broadcast_limit to 50000

--- lib/social/service/facebook.php
+++ lib/social/service/facebook.php
@@ -18,7 +18,7 @@ final class Social_Service_Facebook extends Social_Service implements Social_Int
 	 * @return int
 	 */
 	public function max_broadcast_length() {
-		return 25000;
+		return 50000;
 	}
 
 	/**
